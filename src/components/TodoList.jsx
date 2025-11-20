49ee2cccd878f871f8c0cdfd5c9590935c01aa5c
Ayomide Aina <joytife8@gmail.com>
2025-11-20 06:42:02 +0100

    fix filters

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -13,27 +13,36 @@ const TodoList = ({
 }) => (
   <>
     <section className="container">
-      <ul id="task-list">
+      <div className="task-box">
         {todos.length === 0 && (
-          <div id="empty-state" className="empty-state">
-            <FontAwesomeIcon icon={faNoteSticky} />
+          <div
+            id="empty-state"
+            className="empty-state"
+            style={{ display: "flex" }}
+          >
+            <FontAwesomeIcon icon={faNoteSticky} className="empty-icon" />
             <p>No tasks yet. Add one above to get started!</p>
           </div>
         )}
 
-        {todos.map((todo) => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onEdit={onEdit}
-            onDelete={onDelete}
-            onToggle={onToggle}
-          />
-        ))}
-      </ul>
+        {todos.length > 0 && (
+          <ul id="task-list">
+            {todos.map((todo) => (
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                onEdit={onEdit}
+                onDelete={onDelete}
+                onToggle={onToggle}
+              />
+            ))}
+          </ul>
+        )}
+      </div>
     </section>
-    <footer className="container">
-      <div className="filters">
+
+    <footer className="container task-footer">
+      <div className="filters ">
         {["All", "Active", "Completed"].map((f) => (
           <button
             key={f}
